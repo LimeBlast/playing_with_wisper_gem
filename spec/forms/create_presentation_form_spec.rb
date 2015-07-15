dbc92c5b994c4645013f21b76ba2618f63c257f5
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-07-15 15:37:03 +0100

    added validation specs

--- spec/forms/create_presentation_form_spec.rb
+++ spec/forms/create_presentation_form_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 require 'active_model'
 require 'virtus'
 require 'rspec-virtus'
+require 'shoulda-matchers'
 
 require_relative '../../app/forms/create_presentation_form'
 
 RSpec.describe CreatePresentationForm do
 
   describe 'attributes' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:who) }
+    it { should validate_presence_of(:action) }
+    it { should validate_presence_of(:baggage) }
+    it { should validate_presence_of(:important) }
+    it { should validate_presence_of(:plan) }
+
     it 'has title as a String' do
       expect(described_class).to have_attribute(:title).of_type(String)
     end
