2f891d614b00aa4b1cc2cc2b2e5d846d1e7e337e
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-07-24 14:16:23 +0100

    I don't like it, I just don't like it

--- app/commands/update_presentation.rb
+++ app/commands/update_presentation.rb
@@ -1,19 +1,26 @@
 class UpdatePresentation
   include Wisper::Publisher
 
-  attr_reader :form, :presentation
+  attr_reader :form, :id
 
-  def initialize(form, presentation)
-    @form         = form
-    @presentation = presentation
+  def initialize(form, id)
+    @form = form
+    @id   = id
   end
 
   def call
-    if form.valid?
-      presentation.update_attributes(form.attributes)
-      broadcast(:successful, presentation)
-    else
-      broadcast(:failed, form)
+    command = FindPresentation.new(id)
+    command.on(:successful) do |presentation|
+      if form.valid?
+        presentation.update_attributes(form.attributes)
+        broadcast(:successful, presentation)
+      else
+        broadcast(:failed, form, presentation)
+      end
     end
+    command.on(:failure) do
+      broadcast(:failed)
+    end
+    command.call
   end
 end
